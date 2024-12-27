f631d2e401a1d8832e08078aca1273c51318edf4
25elias <eliasusman83@gmail.com>
2024-12-27 09:28:57 +0300

    Menu bar

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ let btnSlideRight = document.getElementById("btn-slider_right");
 
 let imgItem = document.querySelectorAll(".image-item")
 
-console.log(imgItem)
+// console.log(imgItem)
 
 let startSlider = 0
 let endSlider = (imgItem.length - 1) * 100
 
-console.log(endSlider)
+// console.log(endSlider)
 
 btnSlideLeft.addEventListener("click", ()=> {
     // alert("You just clicked left!")
@@ -20,7 +20,7 @@ btnSlideLeft.addEventListener("click", ()=> {
     })
     console.log(startSlider)
 })
-btnSlideRight.addEventListener("click", ()=> {
+btnSlideRight.addEventListener("click", () => {
     // alert("You just clicked right!")
     if (startSlider >= -endSlider + 100) {
         startSlider = startSlider - 100
@@ -30,3 +30,29 @@ btnSlideRight.addEventListener("click", ()=> {
     })
     console.log(startSlider)
 })
+
+
+
+/*  slide bar show-menu  */
+
+const mobileMenu = document.getElementById("mobile-menu");
+const sideBarOpen = document.getElementById("open-toggle");
+const sideBarClose = document.getElementById("close-toggle");
+
+// if(sideBarOpen) {
+//     sideBarOpen.addEventListener("click", () => {
+//         mobileMenu.classList.add("show-menu")
+//     });
+// }
+sideBarOpen.addEventListener("click", () => {
+    mobileMenu.classList.toggle("show-menu")
+})
+
+// console.log(mobileMenu)
+
+if(sideBarClose) {
+    sideBarClose.addEventListener("click", () => {
+        mobileMenu.classList.remove("show-menu")
+});
+}
+
